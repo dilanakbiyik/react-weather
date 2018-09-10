a8553d90d7eb104982110cce770e32aa26831044
dilanakbiyik <dilanyavuz.93@gmail.com>
2018-09-10 19:59:10 +0200

    Load user location with geo location api. Show location in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+    }
+
+    componentWillMount() {
+        this.setState({
+            location: null
+        })
+    }
+
+    componentDidMount(){
+        this.checkLocation();
+    }
+
+    checkLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.setState({
+                    location: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    }
+                });
+            }, (error) => console.log(error) );
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {JSON.stringify(this.state.location)}
+            </div>
+        );
+    }
 }
 
 export default App;
