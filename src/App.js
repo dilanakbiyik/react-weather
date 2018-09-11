381bd7c010c4b195a528e6c8804a68147f049dfb
dilanakbiyik <dilanyavuz.93@gmail.com>
2018-09-11 19:28:54 +0200

    Load forecast data wıth current location. Seperate days and hours. Show all hours in content.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Search from './components/Search'
+import { forecast } from './services/open-weather-map/open-weather-map.services'
 
 class App extends Component {
     constructor(props){
@@ -9,7 +10,8 @@ class App extends Component {
 
     componentWillMount() {
         this.setState({
-            location: null
+            error: false,
+            days: []
         })
     }
 
@@ -17,24 +19,68 @@ class App extends Component {
         this.checkLocation();
     }
 
+    loadForecast(query) {
+        forecast(query)
+            .then((result) => {
+                let days = {};
+                result.list.forEach((hourInfo) => {
+                    const dateInfos = hourInfo.dt_txt.split(' ');
+                    const hourInfos = dateInfos[1].split(':');
+                    const hourIndex = parseInt(hourInfos[0],0)/3;
+                    const dateName = dateInfos[0];
+                    if(days[dateName]){
+
+                    }else{
+                        days[dateName] = {
+                            data: {}
+                        }
+                    }
+                    days[dateName].data[hourIndex] = hourInfo;
+                });
+                this.setState({ days })
+            })
+            .catch((error) => this.setState({ error : true }))
+    }
+
+    loadHours(){
+        const hours = [0,1,2,3,4,5,6,7];
+        const result = hours.map((val) =>
+            <div className="hour-head" key={'hours-head' + val}>
+                <span>{this.getHourValue(val)}</span>
+            </div>
+        );
+        return result;
+    }
+
+    getHourValue(i){
+        if((i*3) < 10){
+            return `0${i*3}.00`;
+        }else{
+            return `${i*3}.00`;
+        }
+    }
+
     checkLocation(){
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition((position) => {
-                this.setState({
-                    location: {
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude
-                    }
-                });
+                const location = {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                };
+                this.loadForecast(location);
             }, (error) => console.log(error) );
         }
     }
 
     render() {
+        const { days } = this.state;
         return (
             <div className="App">
-                {JSON.stringify(this.state.location)}
                 <Search />
+                <div className="hours-area">
+                    <div className="hour-head"></div>
+                    {this.loadHours()}
+                </div>
             </div>
         );
     }
